b3811ca21a53b6fbcfd204258dba84f43324f419
Bader AlAttar <baderalattar22@gmail.com>
2023-02-10 00:43:55 -0800

    init

---
+++ mask/cli.py
@@ -0,0 +1,16 @@
+import click
+
+@click.command
+@click.argument(
+    'path',
+    # help="Path to image file"
+)
+def cli():
+    '''
+    Mask a square region of the image according 
+    to an RGB value of any other point in the image.
+
+    PATH: the file path of the image.
+    
+    '''
+    pass
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="socket-sandbox",
+    version="0.0.1",
+    author="Bader AlAttar",
+    description="image mask",
+    python_requires=">=3.10",
+    packages=find_packages(),
+    package_data = {
+        "": ["*"],
+    },
+    entry_points={
+        "console_scripts": [
+            "mask=mask.__main__:main",
+        ],
+    },
+)
